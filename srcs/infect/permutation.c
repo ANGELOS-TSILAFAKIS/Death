06e204815e2d9ef254b93fd59e074699fcf285b3
grumbach <anselmega@gmail.com>
2019-12-14 19:10:34 +0100

    [ADD] disasm and permut interface

--- srcs/infect/permutation.c
+++ srcs/infect/permutation.c
@@ -1,29 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   polymorphism.h                                     :+:      :+:    :+:   */
+/*   permutation.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anselme <anselme@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/12/11 20:01:16 by anselme           #+#    #+#             */
-/*   Updated: 2019/12/14 19:08:38 by anselme          ###   ########.fr       */
+/*   Created: 2019/12/13 09:30:34 by anselme           #+#    #+#             */
+/*   Updated: 2019/12/14 18:48:10 by anselme          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef POLYMORPHISM_H
-# define POLYMORPHISM_H
+#include <stdbool.h>
+#include <stddef.h>
 
-# include <stdbool.h>
-# include <stdint.h>
-# include <stddef.h>
-
-# include "famine.h"
-
-bool		yield_seed_to_heir(uint64_t father_seed[2], uint64_t son_seed[2]);
-
-bool		generate_cypher(char *buffer, uint64_t seed, size_t size);
-bool		generate_decypher(char *buffer, uint64_t seed, size_t size);
-
-bool		permutate_instructions(void *buffer, size_t size);
-
-#endif
+bool		permutate_instructions(void *buffer, size_t size)
+{
+	return true;
+}
