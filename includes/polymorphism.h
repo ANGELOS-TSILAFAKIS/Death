2332f55d9e8aaf104002060ace43e172d29c5edd
Sylvie POLOWY <spolowy@student.42.fr>
2020-01-13 20:51:03 +0100

    [WIP] rewrite generate_cypher

--- includes/polymorphism.h
+++ includes/polymorphism.h
@@ -6,7 +6,7 @@
 /*   By: anselme <anselme@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/11 20:01:16 by anselme           #+#    #+#             */
-/*   Updated: 2019/12/27 02:10:35 by anselme          ###   ########.fr       */
+/*   Updated: 2020/01/13 20:42:44 by spolowy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@
 
 uint64_t	polymorphic_seed_engine(uint64_t son_seed[2], uint64_t client_id);
 
-bool		generate_cypher(char *buffer, uint64_t seed, size_t size);
-bool		generate_decypher(char *buffer, uint64_t seed, size_t size);
+bool		generate_cypher(void *buffer, uint64_t seed, size_t size);
+bool		generate_decypher(void *buffer, uint64_t seed, size_t size);
 
 bool		permutate_instructions(void *buffer, uint64_t seed, size_t size);
 bool		permutate_registers(void *buffer, uint64_t seed, size_t size);
